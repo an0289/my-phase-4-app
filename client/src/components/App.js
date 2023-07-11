bde8b7e396b16836d70230cd9bf591d626d8dd6d
Alexandra <alexandraneff@live.com>
2023-07-10 22:12:32 -0500

    added semantic ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,22 +4,27 @@ import NavBar from "./NavBar"
 import Login from "../pages/Login"
 import ItemList from "../pages/ItemList"
 import NewReview from "../pages/NewReview"
+import Test from "./Test"
+import { Menu, Container, Button, Header } from 'semantic-ui-react'
 
 function App() {
   return (
-   <>
-      <NavBar />
-      <main>
-        <Routes>
-          <Route path="/new"
-            element={<NewReview />}
-          />
-          <Route path="/"
-            element={<ItemList />}
-          />
-        </Routes>
-      </main>
-   </>
+   <div>
+   {/* {/* <Header >
+    Hotdogs 
+    <a href='espn.com'>Link</a>
+   </Header>
+   <NavBar />
+
+      <Routes>
+        <Route path="/new"
+          element={<NewReview />}
+        />
+        <Route path="/"
+          element={<ItemList />}
+        />
+      </Routes> */}
+   </div>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import { Menu, Header} from 'semantic-ui-react'
 
 function NavBar() {
-
+<div>
+    Hello
+    {/* <Header>
+    Hi
+    </Header>
+    <Menu.Item as={NavLink} to='/' name='home'>
+        Home
+    </Menu.Item>
+    <Menu.Item as={NavLink} to='/new' name='new review' /> */}
+    
+</div>
 }
 
 export default NavBar
